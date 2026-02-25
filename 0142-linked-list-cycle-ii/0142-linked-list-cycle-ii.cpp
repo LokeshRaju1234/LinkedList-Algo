597583094f6a36ae9a9c2ac9129fa23961999b11
LokeshRaju <loki65628@gmail.com>
2026-02-25 21:57:10 +0530

    Time: 6 ms (72.44%), Space: 11.3 MB (54.23%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,31 +9,25 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-         if(head == nullptr)
-         {
-            return nullptr;
-         }
-         ListNode* slow = head;
-         ListNode* fast = head;
-
-         while(fast != nullptr && fast->next != nullptr)
-         {
-            slow = slow->next;
+        while(fast!= NULL && fast->next != nullptr)
+        {
+            slow  = slow->next;
             fast = fast->next->next;
 
             if(slow == fast)
             {
-                ListNode* fast = head;
-
-                while(fast != slow)
+                slow = head;
+                while(slow != fast)//if they meet again the meet point is where cycle begins
                 {
-                    fast = fast->next;
                     slow = slow->next;
+                    fast = fast->next;
                 }
                 return slow;
             }
-         }
-         return {};
+        }
+        return {};
     }
 };
